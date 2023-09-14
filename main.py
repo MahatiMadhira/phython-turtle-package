9eca9e2fcdf89c755fcbddb77af37b048e48b6d3
Mahati Madhira <mm12032@nyu.edu>
2023-09-14 15:47:38 -0400

    Testing different methods and attributes of PrettyTable package

--- main.py
+++ main.py
@@ -14,9 +14,18 @@ for i in range(0, 12):
     timmy1.right(30)
     timmy2.left(30)
 
-
-
-
-
 myscreen = Screen()
 myscreen.exitonclick()
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander", "Combee", "Pangoro"])
+table.add_column("Type", ["Electric", "Water", "Fire", "Flying", "Dark"])
+table.align = "l"
+print(table)
+
+
+
